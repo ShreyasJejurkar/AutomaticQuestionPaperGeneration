668f0a684023cb98971f1634b849fece9334b0bf
MCCshreyas <shreyasjejurkar123@live.com>
2018-10-16 22:35:45 +0530

    Added new Dignosis packages

--- AutomatedQuestionPaper/Global.asax.cs
+++ AutomatedQuestionPaper/Global.asax.cs
@@ -1,10 +1,12 @@
-﻿using System.Web.Mvc;
+﻿using System.Web;
+using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using System.Web.UI;
 
 namespace AutomatedQuestionPaper
 {
-    public class MvcApplication : System.Web.HttpApplication
+    public class MvcApplication : HttpApplication
     {
         protected void Application_Start()
         {
