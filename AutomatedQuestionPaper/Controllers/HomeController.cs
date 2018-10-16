52e82574171ed8a39eca9fb75d8b6cda03abe051
MCCshreyas <shreyasjejurkar123@live.com>
2018-10-16 22:35:27 +0530

    Added code to end sessions

--- AutomatedQuestionPaper/Controllers/HomeController.cs
+++ AutomatedQuestionPaper/Controllers/HomeController.cs
@@ -5,8 +5,10 @@ namespace AutomatedQuestionPaper.Controllers
     public class HomeController : Controller
     {
         [HttpGet]
+        [Route("/home")]
         public ActionResult Index()
         {
+            Session["Username"] = null;
             return View();
         }   
     }
