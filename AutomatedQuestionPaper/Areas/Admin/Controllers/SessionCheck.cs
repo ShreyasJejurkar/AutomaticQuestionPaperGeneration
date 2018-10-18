f53c4f2656ce4f5d7706698b88103e3380015d67
MCCshreyas <shreyasjejurkar123@live.com>
2018-10-18 14:53:29 +0530

    Added Session management class

---
+++ AutomatedQuestionPaper/Areas/Admin/Controllers/SessionCheck.cs
@@ -0,0 +1,20 @@
+﻿using System.Web.Mvc;
+
+namespace AutomatedQuestionPaper.Areas.Admin.Controllers
+{
+    public class SessionCheck : ActionFilterAttribute
+    {
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            var session = filterContext.HttpContext.Session;
+
+            if (session["Username"] == null)
+            {
+                var uriHelper = new UrlHelper(filterContext.RequestContext);
+                var redirectUrl = uriHelper.Action("Index", "Account", new { area = "" });
+                filterContext.Result = new RedirectResult(redirectUrl);
+            }
+            base.OnActionExecuting(filterContext);
+        }
+    }
+}
