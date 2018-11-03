58ec1f206ddc32bb66c302e28db117b91d76cf53
MCCshreyas <shreyasjejurkar123@live.com>
2018-11-03 20:20:37 +0530

    Added session check for staff

---
+++ AutomatedQuestionPaper/Areas/Staff/SessionCheckStaff.cs
@@ -0,0 +1,29 @@
+﻿using System.Web.Mvc;
+
+namespace AutomatedQuestionPaper.Areas.Staff
+{
+    /// <inheritdoc />
+    /// <summary>
+    ///     Will check for session of login functionality. Should be apply on controller
+    /// </summary>
+    public class SessionCheckStaff : ActionFilterAttribute
+    {
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            // Get the session value of current HTTP Context
+            var session = filterContext.HttpContext.Session;
+            
+            // Check for Username Key value in Session
+            // If its null the redirect request to Account Controller in Index action
+            if (session["Staff_Name"] == null)
+            {
+                var uriHelper = new UrlHelper(filterContext.RequestContext);
+                var redirectUrl = uriHelper.Action("Index", "Account", new {area = ""});
+                filterContext.Controller.TempData["SessionErrorMessage"] = "Please log in to your account first";
+                filterContext.Result = new RedirectResult(redirectUrl);
+            }
+
+            base.OnActionExecuting(filterContext);
+        }
+    }
+}
