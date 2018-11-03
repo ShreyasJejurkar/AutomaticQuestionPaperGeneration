025a2eda6784e372cb851263df87dc48eb9f311d
MCCshreyas <shreyasjejurkar123@live.com>
2018-11-03 20:20:21 +0530

    Added index logic to retrive info about staff

--- AutomatedQuestionPaper/Areas/Staff/Controllers/StaffHomePageController.cs
+++ AutomatedQuestionPaper/Areas/Staff/Controllers/StaffHomePageController.cs
@@ -1,13 +1,23 @@
-﻿using System.Web.Mvc;
+﻿using System.Linq;
+using System.Web.Mvc;
+using AutomatedQuestionPaper.Models;
 
 namespace AutomatedQuestionPaper.Areas.Staff.Controllers
 {
+    [SessionCheckStaff]
     public class StaffHomePageController : Controller
     {
-        // GET: Staff/StaffHomePage
+        private readonly DatabaseContext _context = new DatabaseContext();
+        
+        [HttpGet]
         public ActionResult Index()
         {
-            return View();
+            var staffName = Session["Staff_Name"];
+
+            // Get the admin details from database
+            var staff = _context.Staffs.FirstOrDefault(u => u.Name == (string)staffName);
+
+            return View(staff);
         }
     }
 }
