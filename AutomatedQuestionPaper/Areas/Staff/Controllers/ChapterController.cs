bf79c04d2f60b3be1311ae1f755cf0ecf0388e90
MCCshreyas <shreyasjejurkar123@live.com>
2018-11-04 11:35:16 +0530

    Added chapter controller

---
+++ AutomatedQuestionPaper/Areas/Staff/Controllers/ChapterController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AutomatedQuestionPaper.Areas.Staff.Controllers
+{
+    public class ChapterController : Controller
+    {
+        // GET: Staff/Chapter
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
