cf70a484786b3487629558ac243d552dff980bf6
MCCshreyas <shreyasjejurkar123@live.com>
2018-11-05 17:56:27 +0530

    Added views and controller for View Subject functionality

---
+++ AutomatedQuestionPaper/Areas/Staff/Models/ChapterDetails.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AutomatedQuestionPaper.Areas.Staff.Models
+{
+    public class ChapterDetails
+    {
+        public int? UnitNo { get; set; }
+        public int? ChapterNo { get; set; }
+        public string ChapterName { get; set; }
+    }
+}
