621e9df879e7ba80818b7fef90cbe243f9b6c921
MCCshreyas <shreyasjejurkar123@live.com>
2018-11-06 17:36:18 +0530

    Added functionality for Chapter View,Edit & delete

--- AutomatedQuestionPaper/Areas/Staff/Controllers/ChapterController.cs
+++ AutomatedQuestionPaper/Areas/Staff/Controllers/ChapterController.cs
@@ -10,6 +10,10 @@ namespace AutomatedQuestionPaper.Areas.Staff.Controllers
     [SessionCheckStaff]
     public class ChapterController : Controller
     {
+        private int? _semId, _deptId, _subId;
+
+
+
         private readonly DatabaseContext _context = new DatabaseContext();
 
         // GET: Staff/Chapter
@@ -19,14 +23,14 @@ namespace AutomatedQuestionPaper.Areas.Staff.Controllers
         }
 
         [HttpPost]
-        public ActionResult AddChapter(string selectedSemester,string selectedDepartment, string selectedSubject, string selectedUnit, string chapterNumber, string chapterName)
+        public ActionResult AddChapter(string selectedSemester, string selectedDepartment, string selectedSubject, string selectedUnit, string chapterNumber, string chapterName)
         {
             // Get the Id of selected subject
-            var subjectId = _context.Courses.FirstOrDefault(u=>u.CourseName == selectedSubject)?.Courseid;
+            var subjectId = _context.Courses.FirstOrDefault(u => u.CourseName == selectedSubject)?.Courseid;
 
 
             // Get the Id of selected department 
-            var departmentId = _context.Departments.FirstOrDefault(u=>u.DepartmentName == selectedDepartment)?.Id;
+            var departmentId = _context.Departments.FirstOrDefault(u => u.DepartmentName == selectedDepartment)?.Id;
 
             // Get the Id of selected semester 
             var semesterId = _context.Semesters.FirstOrDefault(u => u.SemesterName == selectedSemester)?.Id;
@@ -56,7 +60,7 @@ namespace AutomatedQuestionPaper.Areas.Staff.Controllers
             var staffId = _context.Staffs.FirstOrDefault(u => u.Name == staffName)?.Id;
 
             var semesterIDs = _context.StaffCourses.Where(u => u.StaffId == staffId).Select(u => u.SemesterId).Distinct().ToList(); ;
-           
+
             var semesterName = new List<string>();
 
             foreach (var id in semesterIDs)
@@ -72,10 +76,16 @@ namespace AutomatedQuestionPaper.Areas.Staff.Controllers
         [HttpGet]
         public ActionResult GetAllocatedDepartments(string semesterName)
         {
+            var name = (string)Session["Staff_Name"];
+
+            // Get the logged staff Id
+            var staffId = _context.Staffs.FirstOrDefault(u => u.Name == name)?.Id;
+
+
             // Get the semester Id
-            var semID = _context.Semesters.FirstOrDefault(u=>u.SemesterName == semesterName)?.Id;
-            
-            var departmentIDs = _context.StaffCourses.Where(u => u.SemesterId == semID).Select(u => u.DepartmentId).Distinct().ToList();
+            var semID = _context.Semesters.FirstOrDefault(u => u.SemesterName == semesterName)?.Id;
+
+            var departmentIDs = _context.StaffCourses.Where(u => u.SemesterId == semID && u.StaffId == staffId).Select(u => u.DepartmentId).Distinct().ToList();
 
             var departmentName = new List<string>();
 
@@ -85,18 +95,18 @@ namespace AutomatedQuestionPaper.Areas.Staff.Controllers
                 departmentName.Add(deptName);
             }
 
-            return Json(departmentName,JsonRequestBehavior.AllowGet);
+            return Json(departmentName, JsonRequestBehavior.AllowGet);
         }
 
         [HttpGet]
-        public ActionResult GetSubjectsList(string departmentName , string semesterName)
+        public ActionResult GetSubjectsList(string departmentName, string semesterName)
         {
             // Get the selected department Id
             var deptId = _context.Departments.FirstOrDefault(u => u.DepartmentName == departmentName)?.Id;
-            
+
             // Get the selected sem Id
             var semId = _context.Semesters.FirstOrDefault(u => u.SemesterName == semesterName)?.Id;
-            
+
             var subjectIDs = _context.StaffCourses.Where(u => u.DepartmentId == deptId && u.SemesterId == semId).Select(u => u.CourseId).Distinct().ToList();
 
             var subjectsName = new List<string>();
@@ -121,29 +131,105 @@ namespace AutomatedQuestionPaper.Areas.Staff.Controllers
             // Get the Id of selected subject
             var subjectId = _context.Courses.FirstOrDefault(u => u.CourseName == selectedSubject)?.Courseid;
 
+            TempData["Data_SubjectId"] = subjectId;
 
             // Get the Id of selected department 
             var departmentId = _context.Departments.FirstOrDefault(u => u.DepartmentName == selectedDepartment)?.Id;
 
+            TempData["Data_DepartmentId"] = departmentId;
+
             // Get the Id of selected semester 
             var semesterId = _context.Semesters.FirstOrDefault(u => u.SemesterName == selectedSemester)?.Id;
-            
-            var data =  _context.Chapters.Where(u =>
-                    u.SemesterId == semesterId && u.DepartmentId == departmentId && u.CourseId == subjectId)
-                .Select(x => new {x.UnitNo, x.ChapterNo, x.ChapterName}).ToList();
-            
+
+            TempData["Data_SemesterId"] = semesterId;
+
+            var data = _context.Chapters.Where(u =>
+                   u.SemesterId == semesterId && u.DepartmentId == departmentId && u.CourseId == subjectId)
+                .Select(x => new { x.UnitNo, x.ChapterNo, x.ChapterName }).ToList();
+
             var list = data.Select(x => new ChapterDetails
             {
                 UnitNo = x.UnitNo,
                 ChapterName = x.ChapterName,
                 ChapterNo = x.ChapterNo
-            }).OrderBy(x=>x.UnitNo).ToList();
+            }).OrderBy(x => x.UnitNo).ToList();
 
-            
+
             TempData["ListOfChapters"] = list;
 
 
-            return View("ViewChapters",list);
+            return View("ViewChapters", list);
+        }
+
+        [HttpGet]
+        public ActionResult EditChapterDetails(string name)
+        {
+            var chapterId = _context.Chapters.FirstOrDefault(u => u.ChapterName == name)?.Id;
+
+            var chapterInfo = _context.Chapters.FirstOrDefault(u => u.Id == chapterId);
+
+            var semName = _context.Semesters.FirstOrDefault(u => u.Id == chapterInfo.SemesterId);
+
+            var depName = _context.Departments.FirstOrDefault(u => u.Id == chapterInfo.DepartmentId);
+
+            TempData["SemName"] = semName.SemesterName;
+            TempData["depName"] = depName.DepartmentName;
+            TempData["UnitNo"] = chapterInfo.UnitNo;
+
+            return View(chapterInfo);
+        }
+
+        [HttpPost]
+        public ActionResult EditChapterDetails(Chapter chap, string selectedSemester, string selectedDepartment, string selectedUnit)
+        {
+            // Get a corresponding chapter from db
+            var dbChapter = _context.Chapters.FirstOrDefault(u=>u.Id == chap.Id);
+
+            // Get the semester Id
+            var semesterId = _context.Semesters.FirstOrDefault(u=>u.SemesterName == selectedSemester)?.Id;
+
+            // Get the department Id
+            var departmentId = _context.Departments.FirstOrDefault(u => u.DepartmentName== selectedDepartment)?.Id;
+
+            chap.DepartmentId = departmentId;
+            chap.SemesterId = semesterId;
+            chap.UnitNo = Convert.ToInt32(selectedUnit);
+            
+            dbChapter.ChapterName = chap.ChapterName;
+            dbChapter.DepartmentId = chap.DepartmentId;
+            dbChapter.SemesterId = chap.SemesterId;
+            dbChapter.UnitNo = chap.UnitNo;
+            dbChapter.ChapterNo = chap.ChapterNo;
+            _context.SaveChangesAsync();
+
+
+            TempData["ChapterDetailsEditedSuccessfully"] = "Chapter details edited successfully";
+
+            return RedirectToAction("ViewChapters");
+        }
+
+
+
+        public ActionResult DeleteChapter(string name)
+        {
+            var subjectId = (int?)TempData["Data_SubjectId"];
+            var departmentId = (int?)TempData["Data_DepartmentId"];
+            var semesterId = (int?) TempData["Data_SemesterId"];
+
+            var subject = _context.Chapters.FirstOrDefault(x =>
+                x.ChapterName == name && x.SemesterId == semesterId && x.DepartmentId == departmentId &&
+                x.CourseId == subjectId);
+            if (subject != null)
+            {
+                _context.Chapters.Remove(subject);
+                _context.SaveChangesAsync();
+
+                TempData["ChapterDetailsDeletedSuccessfully"] = "Chapter deleted successfully";
+
+                return RedirectToAction("ViewChapters");
+            }
+
+            return RedirectToAction("ViewChapters");
         }
     }
 }
