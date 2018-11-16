5981e4408a01561941fa09aab7d5010df52af952
MCCshreyas <shreyasjejurkar123@live.com>
2018-11-16 22:51:46 +0530

    Added ExamType Enum

---
+++ AutomatedQuestionPaper/Models/ExamType.cs
@@ -0,0 +1,15 @@
+﻿namespace AutomatedQuestionPaper.Models
+{
+    /// <summary>
+    /// Represent the exam type for question
+    /// </summary>
+    public enum ExamType
+    {
+        
+        /// A 30 marks paper
+        InSem = 0,
+        
+        /// A 70 marks paper
+        EndSem = 1
+    }
+}
