c5bc3431782729462a3c5496cea54736a0f0fa25
MCCshreyas <shreyasjejurkar123@live.com>
2018-12-16 20:02:01 +0530

    Added BackButton js file

---
+++ AutomatedQuestionPaper/Scripts/custom/BackButton.js
@@ -0,0 +1,16 @@
+﻿
+
+function DisableBackButton() {
+    window.history.forward();
+}
+
+DisableBackButton();
+window.onload = DisableBackButton;
+window.onpageshow = function(evt) {
+    if (evt.persisted) DisableBackButton();
+
+};
+window.onunload = function() {
+    void (0);
+
+};
