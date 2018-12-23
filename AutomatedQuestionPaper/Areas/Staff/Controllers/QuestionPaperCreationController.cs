3c97ab3f6f60fd6b83d2a996d63e1d4fc0f203e3
MCCshreyas <shreyasjejurkar123@live.com>
2018-12-23 12:02:49 +0530

    Added new controller for quesion paper creation

---
+++ AutomatedQuestionPaper/Areas/Staff/Controllers/QuestionPaperCreationController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AutomatedQuestionPaper.Areas.Staff.Controllers
+{
+    public class QuestionPaperCreationController : Controller
+    {
+        // GET: Staff/QuestionPaperCreation
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
