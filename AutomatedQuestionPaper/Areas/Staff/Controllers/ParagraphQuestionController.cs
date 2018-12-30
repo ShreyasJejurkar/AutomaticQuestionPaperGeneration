791558122d4d19d1021a1cc22ae9982a225f1e88
MCCshreyas <shreyasjejurkar123@live.com>
2018-12-30 20:07:20 +0530

    added controller for paragraph question creater

---
+++ AutomatedQuestionPaper/Areas/Staff/Controllers/ParagraphQuestionController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AutomatedQuestionPaper.Areas.Staff.Controllers
+{
+    public class ParagraphQuestionController : Controller
+    {
+        // GET: Staff/ParagraphQuestion
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
