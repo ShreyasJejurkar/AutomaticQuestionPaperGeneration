d812be1919690da1911ce44206fdc289e2587570
MCCshreyas <shreyasjejurkar123@live.com>
2018-12-30 20:06:36 +0530

    Added method for converting word to pdf

--- AutomatedQuestionPaper/Areas/Staff/Models/PDFHandler.cs
+++ AutomatedQuestionPaper/Areas/Staff/Models/PDFHandler.cs
@@ -1,53 +1,75 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Data.SqlClient;
-using System.Diagnostics;
+﻿using System.Data.SqlClient;
 using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using AutomatedQuestionPaper.Models;
+using Spire.Doc;
 
 namespace AutomatedQuestionPaper.Areas.Staff.Models
 {
     public static class PdfHandler
     {
-        public static void SavePdf()
+        public static void ConvertToPdf(string questionPaperPath)
         {
-            DatabaseContext context = new DatabaseContext();
+            var d = new Document();
+            d.LoadFromFile(questionPaperPath);
+
+            var pathToSavePdf = HttpContext.Current.Server.MapPath("~/App_Data/PdfQuestionPapers/sample.pdf");
+
+            d.SaveToFile(pathToSavePdf, FileFormat.PDF);
+
+           SaveQuestionPaper(pathToSavePdf, questionPaperPath);
+        }
+
+        private static void SaveQuestionPaper(string pathPdf, string pathWord)
+        {
+            var context = new DatabaseContext();
 
             using (var con = new SqlConnection(context.Database.Connection.ConnectionString))
             {
                 con.Open();
-                var fileStream = File.OpenRead(HttpContext.Current.Server.MapPath($"~/Areas/Staff/Uploads/sample.pdf"));
-                byte[] content = new byte[fileStream.Length];
-                fileStream.Read(content, 0, (int)fileStream.Length);
+                var fileStream = File.OpenRead(pathPdf);
+                byte[] contentPdf = new byte[fileStream.Length];
+                fileStream.Read(contentPdf, 0, (int)fileStream.Length);
                 fileStream.Close();
+
+                var fileStream1 = File.OpenRead(pathWord);
+                byte[] contentWord = new byte[fileStream1.Length];
+                fileStream1.Read(contentWord, 0, (int)fileStream1.Length);
+                fileStream1.Close();
 
                 context.ExamPapers.Add(new ExamPaper
                 {
-                    PaperName = "sample.pdf",
-                    StaffId = 2,
-                    PaperValue = content
+                    // TODO Create the name of paper as per semester department and subject
+                    PaperName = "test",
+
+                    // TODO Get the logged in staff id and assign it here
+                    StaffId = 10,
+
+                    PaperValue = contentPdf,
+
+                    PaperValueWord = contentWord
                 });
 
                 context.SaveChanges();
-
-                Debug.Write("Done");
-
             }
         }
 
         public static FileStreamResult DisplayPdf(int id)
         {
-            DatabaseContext context = new DatabaseContext();
-            var fileByteData = context.ExamPapers.FirstOrDefault(i => i.Id == id).PaperValue;
-
-            MemoryStream pdfStream = new MemoryStream();
-            pdfStream.Write(fileByteData, 0, fileByteData.Length);
+            var context = new DatabaseContext();
+            var fileByteData = context.ExamPapers.FirstOrDefault(i => i.Id == id);
+            
+            var pdfStream = new MemoryStream();
+            pdfStream.Write(fileByteData.PaperValue, 0, fileByteData.PaperValue.Length);
             pdfStream.Position = 0;
+            HttpContext.Current.Response.AppendHeader("content-disposition", "attachment; filename=form.pdf");
+
             return new FileStreamResult(pdfStream, "application/pdf");
         }
+
+        // TODO make a method for displaying word file same as DisplayPDf
 
     }
 }
---
+++ AutomatedQuestionPaper/Areas/Staff/Models/WordHandler.cs
@@ -0,0 +1,23 @@
+﻿using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using AutomatedQuestionPaper.Models;
+
+namespace AutomatedQuestionPaper.Areas.Staff.Models
+{
+    public class WordHandler
+    {
+        public static FileStreamResult DownloadWordFile(int id)
+        {
+            var context = new DatabaseContext();
+            var fileByteData = context.ExamPapers.FirstOrDefault(i => i.Id == id);
+
+            var wordStream = new MemoryStream();
+            wordStream.Write(fileByteData.PaperValueWord, 0, fileByteData.PaperValueWord.Length);
+            wordStream.Position = 0;
+            HttpContext.Current.Response.AddHeader("content-disposition", $"attachment; filename={fileByteData.PaperName}.doc");
+            return new FileStreamResult(wordStream, "application/msword");
+        }
+    }
+}
