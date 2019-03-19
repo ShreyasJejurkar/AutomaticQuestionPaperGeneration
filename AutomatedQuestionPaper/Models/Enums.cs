ad175198d8d2e723e0f08ca0353c7ada3c55a471
MCCshreyas <shreyasjejurkar123@live.com>
2019-03-19 21:08:47 +0530

    popup file updated

---
+++ AutomatedQuestionPaper/Models/Enums.cs
@@ -0,0 +1,13 @@
+﻿namespace AutomatedQuestionPaper.Models
+{
+    public class Enums
+    {
+        public enum NotificationType
+        {
+            error,
+            success,
+            warning,
+            info
+        }
+    }
+}
